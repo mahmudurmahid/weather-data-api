ddd50ed4bd3a8561a3e78c24c79068005cc7384f
mahmudurmahid <m.mahid@icloud.com>
2025-04-16 17:15:11 +0100

    changed about page to include station, date, and temp info, added gitignore, added images to static folder, added index and about html pages to templates folder

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    temperature = 23
+    return {"station": station,
+            "date": date,
+            "temperature": temperature
+            }
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
