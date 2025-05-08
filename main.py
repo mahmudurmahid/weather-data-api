9d38573df330776db5331d8734746a6e4341d8c8
mahmudurmahid <m.mahid@icloud.com>
2025-05-08 19:25:14 +0100

    added comments to main.py to explain what the functions do

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ stations = stations[["STAID", "STANAME                                 "]]
 
 @app.route("/")
 def home():
+    """Home page of the web application."""
     return render_template("index.html", data=stations.to_html())
 
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
+    """API endpoint to get temperature data for a specific station and date."""
     filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
     temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
@@ -24,6 +26,7 @@ def about(station, date):
 
 @app.route("/api/v1/<station>")
 def all_data(station):
+    """API endpoint to get all temperature data for a specific station."""
     filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
     result = df.to_dict(orient="records")
@@ -32,6 +35,7 @@ def all_data(station):
 
 @app.route("/api/v1/annual/<station>/<year>")
 def yearly_data(station, year):
+    """API endpoint to get annual temperature data for a specific station and year."""
     filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filename, skiprows=20,)
     df["    DATE"] = df["    DATE"].astype(str)
